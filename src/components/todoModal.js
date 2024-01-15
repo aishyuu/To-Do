054238ec86c5f1ecfe9db77f2b8a67e760d088e1
aishyuu <ivanmatias1331@gmail.com>
2024-01-15 08:08:41 -0800

    Added modal exit button, functionality, and style

--- src/components/todoModal.js
+++ src/components/todoModal.js
@@ -9,6 +9,21 @@ function todoModal(element) {
     modalDiv.classList.add("modal-content-div")
     modalBackdrop.appendChild(modalDiv)
 
+    // X click to finish
+    const modalCloseDiv = document.createElement("div");
+    modalCloseDiv.classList.add("modal-close-div");
+
+    const closeButton = document.createElement("p");
+    closeButton.classList.add("modal-close-button")
+    closeButton.innerText = "x"
+    modalCloseDiv.appendChild(closeButton)
+    modalDiv.appendChild(modalCloseDiv);
+
+    closeButton.addEventListener('click', () => {
+        const entireModal = document.querySelector(".modal-backdrop");
+        entireModal.remove()
+    })
+
     body.appendChild(modalBackdrop)
 }
 
